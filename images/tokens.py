0677c87fcbfff7d8bfece2f83fd9253f8d3a78d3
Njiiri Yvonne <njerinjiiri@gmail.com>
2021-12-15 02:34:26 -0500

    add packages to requirements.txt

--- images/tokens.py
+++ images/tokens.py
@@ -8,7 +8,7 @@ class TokenGenerator(PasswordResetTokenGenerator):
         #     six.text_type(user.is_active)
         # )
         return (
-            text_type(user.pk) + text_type(timestamp) +
-            text_type(user.is_active)
+            text_type(user.pk) + six.text_type(timestamp) +
+            six.text_type(user.is_active)
         )
 account_activation_token = TokenGenerator()
